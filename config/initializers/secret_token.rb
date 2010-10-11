1e99f3bb2e041220cf46048cc49622ea7e097a8c
Pavel Murnikov <pavel@pavelmurnikov.com>
2010-10-11 17:10:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrightText::Application.config.secret_token = '059375f5a6adab3ccbb5fd8929a2c25a6e54bcc33c5f38bfd291a1be302734482ca1d191057a25760c3cc0f74f422b07b1e74123f67b629cc23e5c134f5517bf'
