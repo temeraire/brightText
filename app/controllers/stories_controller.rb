b1c4d063f9c39869c6518157343aa40039d3465b
Olga Andreyeva <inremajor@gmail.com>
2013-04-09 08:33:48 +0400

    Now can save story more than once

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -98,7 +98,7 @@ class StoriesController < ApplicationController
     
     respond_to do |format|
       if @story.save
-        format.json{ render :json=> {:success => "true"} }
+        format.json{ render :json=> {:success => "true", :story_id => @story.id } }
         format.html { redirect_to("/stories?filter=" + @story.story_set_id.to_s, :notice => 'Story was successfully created.') }
         format.xml  { render :xml => @story, :status => :created, :location => @story }
       else
@@ -123,6 +123,7 @@ class StoriesController < ApplicationController
         format.html { redirect_to("/stories?filter=" + @story.story_set_id.to_s, :notice => 'Story was successfully updated.') }
         format.xml  { head :ok }
       else
+        logger.debug "#{@story}"
         format.json{ render :json=> {:success => "false"} }
         format.html { render :action => "edit" }
         format.xml  { render :xml => @story.errors, :status => :unprocessable_entity }
