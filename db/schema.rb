6af4ee5e78b68e3aa6e3801aedbe5921776c3ff9
Olga Andreyeva <inremajor@gmail.com>
2013-06-01 17:25:53 +0400

    password_confirmation added to users

--- db/schema.rb
+++ db/schema.rb
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(:version => 920130109084745) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "lastname"
+    t.integer  "password_confirmation"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
