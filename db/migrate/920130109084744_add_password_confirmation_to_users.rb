bbfcb5293bbbeca77b813a5bafe886b3d34980c1
Olga Andreyeva <inremajor@gmail.com>
2013-06-01 21:39:13 +0400

    migration with password_confirmation removed

--- db/migrate/920130109084744_add_password_confirmation_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddPasswordConfirmationToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :password_confirmation, :string
-  end
-
-  def self.down
-    remove_column :users, :password_confirmation
-  end
-end
