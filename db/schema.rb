037e72d849fd1e301ff016c1d345aef9b1cdbf99
Olga Andreyeva <inremajor@gmail.com>
2013-06-03 10:05:14 +0400

    lastname migration

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(:version => 920130109084745) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "lastname"
-    t.integer  "password_confirmation"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
