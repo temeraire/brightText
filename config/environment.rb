b05f7bd11add1b55b5f0567e30868e8451f766f5
Olga Andreyeva <inremajor@gmail.com>
2013-06-03 09:46:26 +0400

    mail settings changed back

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 BrightText::Application.initialize!
 
-#Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)   
+# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)   
 # ActionMailer::Base.perform_deliveries = true
 # ActionMailer::Base.raise_delivery_errors = true    
 # ActionMailer::Base.delivery_method = :smtp
-# # ActionMailer::Base.smtp_settings = {
+# ActionMailer::Base.smtp_settings = {
 #   :address              => "smtp.gmail.com",  
 #   :port                 => 587,  
 #   :user_name            => 'pavel@brighttext.com',
