7cce1f391eed40c6be069211b28ba38004b62c3f
Asif Tasleem <asiftasleem@gmail.com>
2013-09-20 22:07:42 +0300

    Fixed Text Area issue.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
     // increase the default animation speed to exaggerate the effect
+    /*
 $(document).ready(function() {
 	var table =$(".sortable_header").stupidtable();
 	table.bind('aftertablesort', function (event, data) {
@@ -9,4 +10,4 @@ $(document).ready(function() {
 	    var arrow = data.direction === "asc" ? "&uarr;" : "&darr;";
 	    th.eq(data.column).append('<span class="arrow">' + arrow +'</span>');
 	});
-});
+});*/
