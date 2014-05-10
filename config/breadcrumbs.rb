99ac18c0430e8a2af9899599348bcc8c48da4b1f
Asif <Asif@asif-pc>
2014-05-10 11:34:56 +0300

    breadcrumbs

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,15 +1,12 @@
-crumb :root do
-  link "Home", admin_bright_text_applications_path
-end
-
-crumb :bright_text_application do |bright_text_application|
- link bright_text_application.name, admin_story_set_categories_path
- parent :root
+crumb :root do |bright_text_application|
+  if(bright_text_application.present?)
+    link bright_text_application.name, admin_story_set_categories_path
+  end 
 end
 
 crumb :story_set_category do |story_set_category|
  link story_set_category.name, admin_story_sets_path
- parent :bright_text_application, story_set_category.bright_text_application 
+ parent :root, story_set_category.bright_text_application 
 end
 
 crumb :story_set do |story_set|
