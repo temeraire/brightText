2f6a1f8ee4cca09d2060494e7142834623d196cd
Asif <Asif@asif-pc>
2014-07-04 00:39:26 +0300

    is paid user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,10 +13,12 @@ class Api::UsersController < ActionController::Base
       format.json {  
         if (@user = User.authenticate user_name, password)    
           
-          @user_app = UserApp.where(:user_id=>@user.id, :bright_text_application_id=>application_id)
+          @user_app = UserApp.where(:user_id=>@user.id, :bright_text_application_id=>application_id).first
           
-          if @user_app.platforms[platform]
-            render :json=> { :success => "true"} 
+          if platform == "ios" && @user_app.ios?
+            render :json=> { :success => "true"}
+          elsif platform == "android" && @user_app.android?
+            render :json=> { :success => "true"}
           else
             render :json=> { :success => "false", :message=>"You are not registered for this platform!"}      
           end
